3c6876bf967073d5d2ab539e8084c76b1a5043d6
Mirza Hrvanović <68301043+hrvanovic@users.noreply.github.com>
2021-01-27 22:08:41 +0100

    Create rekurzija_1.cpp

---
+++ rekurzija_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int izracunaj(int, int);
+
+int main() {
+    int broj, eksponent;
+    cout << "Unesite broj i eksponent: ";
+    cin >> broj >> eksponent;
+
+    cout << "Rezultat: " <<  izracunaj(broj, eksponent);
+    return 0;
+}
+
+int izracunaj(int broj, int eksponent) {
+    if(eksponent <= 1)
+        return broj;
+    return broj * izracunaj(broj, eksponent - 1);
+}
