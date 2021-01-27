54f66bf4d7c4c97f7ea8f21240c97d0c6a2066ef
Mirza Hrvanović <68301043+hrvanovic@users.noreply.github.com>
2021-01-27 15:38:16 +0100

    Create procenat.cpp

---
+++ procenat.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+float procenat(int);
+bool prost(int);
+int main() {
+    int n;
+    do {
+        cout << "Unesite broj n: ";
+        cin >> n;
+    } while(n < 10 || n > 1000);
+
+    cout << "Procenat: " << procenat(n);
+
+    return 0;
+}
+bool prost(int broj) {
+    if(broj <= 1)
+        return false;
+
+    for(int i = 2; i <= broj / 2; i++) {
+        if(broj % i == 0)
+            return false;
+    }
+
+    return true;
+}
+float procenat(int n) {
+    int randBr, brojac = 0;
+    for(int i = 1; i <= n; i++) {
+        randBr = rand() % 1000 + 1;
+        cout << i << ". " << " " << randBr << ((prost(randBr)) ? " - Prost je" : " - Nije prost") << endl;
+        if(prost(randBr))
+            brojac++;
+    }
+    return (brojac * 100) / n;
+}
